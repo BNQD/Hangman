b99b4e20c428a620d3c62c584cca481eb4c6c891
Nam Doan <nam.quang.doan@gmail.com>
2018-06-29 16:24:24 -0400

    Basic Hangman Game

--- hangman.rb
+++ hangman.rb
@@ -6,23 +6,27 @@ configure do
 	set :session_secret, "secret"
 end
 
+word = "Jeremy"
+progress = word.gsub /[A-Za-z0-9]/, '-'
+
 get '/' do
-	guess = params["guess"]
-	word = "Jeremy"
-	correct = wordManager(word, guess)
-	progress = word
+	guess = params["guess"].downcase
+	progress = wordManager(word, progress, guess)
+	
 	erb :mainpage, :locals => {
-		:sessionNum => session, :check => correct, :word => word,
+		:sessionNum => session, :word => word,
 		:progress => progress
 	}
 end 
 
-def wordManager (word, guess)
-	letters = word.split('')
+def wordManager (word, progress, guess)
+	letters = word.downcase.split('')
 	if letters.include?(guess)
-		return "Inside!"
-	else
-		return "Not Included"
+		letters.each_with_index do |letter, index|
+			if (letter == guess)
+				progress[index] = letters[index]
+			end
+		end 
 	end
-	return "Bleb"
+	return progress
 end
